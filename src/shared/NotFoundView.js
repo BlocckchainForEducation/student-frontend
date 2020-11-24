1b2c9207a66804ddfe65a18c355d6d56c9b17899
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-11-24 10:48:59 +0700

    :truck: move utils component -> shared

--- src/shared/NotFoundView.js
+++ src/shared/NotFoundView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Container, Typography, makeStyles } from "@material-ui/core";
-import Page from "src/utils/Page";
+import Page from "src/shared/Page";
 
 const useStyles = makeStyles((theme) => ({
   root: {
