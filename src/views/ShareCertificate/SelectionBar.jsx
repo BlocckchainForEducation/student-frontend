fb8b81e00bcf14a2ec5c9c96502a143f9f2ee80a
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-03 20:46:04 +0700

    :construction: SelectionBar: working on event

--- src/views/ShareCertificate/SelectionBar.jsx
+++ src/views/ShareCertificate/SelectionBar.jsx
@@ -1,26 +1,44 @@
 import { Button, makeStyles, Paper, Typography } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getToken } from "../../utils/mng-token";
+import { updateStateOnSelectionChange } from "./redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // display: "flex",
-    // justifyContent: "space-between",
-    // alignItems: "center",
     padding: theme.spacing(2, 2),
   },
 }));
 
 export default function SelectionBar(props) {
   const cls = useStyles();
+  const currentSelectedAccount = useSelector((state) => state.shareCertificateSlice.currentSelectedAccount);
+
   const accounts = useSelector((state) => state.sawtoothAccountsSlice.accounts);
+
+  const dp = useDispatch();
+  async function hdChangeSelection(e, selectedValue) {
+    let response = await fetch(`${process.env.REACT_APP_SERVER_URL}/student/encrypted-data?publicKey=${selectedValue}`, {
+      headers: { "Content-Type": "application/json", Authorization: getToken() },
+    });
+
+    const result = await response.json();
+    if (!response.ok) {
+      console.log(result);
+    } else {
+      dp(updateStateOnSelectionChange(result));
+    }
+  }
+
   return (
     <Paper className={cls.root}>
       <Autocomplete
         options={accounts}
-        getOptionLabel={(account) => account.publicKey}
-        renderInput={(params) => <TextField {...params} label="Chọn tài khoản" variant="outlined" />}
+        getOptionLabel={(account) => `${account.publicKey} - ${account.note}`}
+        renderInput={(params) => (
+          <TextField {...params} label="Chọn tài khoản" variant="outlined" value={currentSelectedAccount} onChange={hdChangeSelection} />
+        )}
       ></Autocomplete>
     </Paper>
   );
