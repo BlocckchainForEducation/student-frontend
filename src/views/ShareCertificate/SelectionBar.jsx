33b04c05efc125a01952ff4694d5634bffe6bd12
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-03 20:05:25 +0700

    :construction: working on UI

--- src/views/ShareCertificate/SelectionBar.jsx
+++ src/views/ShareCertificate/SelectionBar.jsx
@@ -5,17 +5,10 @@ import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
+    // display: "flex",
+    // justifyContent: "space-between",
+    // alignItems: "center",
     padding: theme.spacing(2, 2),
-  },
-  selectBox: {
-    flexGrow: 1,
-    padding: theme.spacing(0, 2),
-  },
-  noShrink: {
-    flexShrink: 0,
   },
 }));
 
@@ -24,21 +17,11 @@ export default function SelectionBar(props) {
   const accounts = useSelector((state) => state.sawtoothAccountsSlice.accounts);
   return (
     <Paper className={cls.root}>
-      <div className={cls.noShrink}>
-        <Typography variant="button">Chon tai khoan</Typography>
-      </div>
-      <div className={cls.selectBox}>
-        <Autocomplete
-          options={accounts}
-          getOptionLabel={(account) => account.publicKey}
-          renderInput={(params) => <TextField {...params} label="Chon tai khoan" variant="outlined" />}
-        ></Autocomplete>
-      </div>
-      <div className={cls.noShrink}>
-        <Button variant="contained" color="primary">
-          Lay du lieu
-        </Button>
-      </div>
+      <Autocomplete
+        options={accounts}
+        getOptionLabel={(account) => account.publicKey}
+        renderInput={(params) => <TextField {...params} label="Chọn tài khoản" variant="outlined" />}
+      ></Autocomplete>
     </Paper>
   );
 }
