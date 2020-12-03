d1598f51e1f1bd7dc7e17fa4fa41a3f7875fda85
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-03 22:31:34 +0700

    :construction: decrypt data: working on event

--- src/views/ShareCertificate/AlertFetchResultBar.jsx
+++ src/views/ShareCertificate/AlertFetchResultBar.jsx
@@ -1,5 +1,8 @@
 import { Box, Button, makeStyles, Paper } from "@material-ui/core";
 import { Alert, AlertTitle } from "@material-ui/lab";
+import { useDispatch, useSelector } from "react-redux";
+import { getToken } from "../../utils/mng-token";
+import { updateStateWhenDecryptedData } from "./redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,6 +15,40 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AlertFetchResultBar(props) {
   const cls = useStyles();
+  const selectedAccount = useSelector((state) => state.shareCertificateSlice.currentSelectedAccount);
+  const encryptData = useSelector((state) => state.shareCertificateSlice.encryptedDataOfAccount);
+  const dp = useDispatch();
+
+  async function hdClick() {
+    let privateKey = selectedAccount.privateKey;
+
+    // if user do not save the private key for that account
+    if (!privateKey) {
+      const result = await askPrivateKeyFromWallet();
+      if (!result.ok) {
+        alert("Bạn cần cung cấp private key để có thể giải mã!");
+        return;
+      } else {
+        privateKey = result.privateKey;
+      }
+    }
+
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/student/decrypt-data`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Authorization: getToken() },
+      body: JSON.stringify({ encryptData, privateKey }),
+    });
+    const result = await response.json();
+    if (!response.ok) {
+      console.log(result);
+    } else {
+      dp(updateStateWhenDecryptedData(result));
+    }
+  }
+
+  async function askPrivateKeyFromWallet() {
+    return Promise.resolve({ ok: true, privateKey: "asdf" });
+  }
 
   return (
     <div>
@@ -23,7 +60,7 @@ export default function AlertFetchResultBar(props) {
             </Alert>
           </Box>
           <Box pl={2} flexShrink={0}>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" onClick={hdClick}>
               Giải mã
             </Button>
           </Box>
